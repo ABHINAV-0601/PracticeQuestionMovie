2f20be9372a8e9042a51f2dddf75730a64d980ef
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-11-02 16:26:49 +0530

    completed the tasks

---
+++ PracticeQuestionMovie/src/main/java/com/question/PracticeQuestionMovie/PracticeQuestionMovieApplication.java
@@ -0,0 +1,13 @@
+package com.question.PracticeQuestionMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeQuestionMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeQuestionMovieApplication.class, args);
+	}
+
+}
---
+++ PracticeQuestionMovie/src/test/java/com/question/PracticeQuestionMovie/PracticeQuestionMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.question.PracticeQuestionMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeQuestionMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
